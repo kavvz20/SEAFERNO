f33194b1e56ae6c443ce852ef7611717c10c6698
manyaa08 <codemewith4@gmail.com>
2025-08-25 14:01:33 +0530

    Added sponsors

--- src/Sponsors.jsx
+++ src/Sponsors.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import bgImage from "./assets/bg.png";
 import rotatedBgImage from "./assets/bgrotrated.png";
-import s2 from "./assets/S2.jpg";
+import s1 from "./assets/S1.png";
+import s2 from "./assets/S2.png";
+import s3 from "./assets/S3.png";
 import s4 from "./assets/S4.png";
-import s5 from "./assets/S5.jpg";
-import s6 from "./assets/S6.jpg";
+import s5 from "./assets/S5.png";
+import s6 from "./assets/S6.png";
 import "./Sponsors.css";
 
 export default function Sponsors() {
@@ -23,7 +25,9 @@ export default function Sponsors() {
           className="sponsors-rotated-bg-image"
         />
         <div className="sponsor-logos">
+          <img src={s1} alt="Sponsor S1" className="sponsor-logo" />
           <img src={s2} alt="Sponsor S2" className="sponsor-logo" />
+          <img src={s3} alt="Sponsor S3" className="sponsor-logo" />
           <img src={s4} alt="Sponsor S4" className="sponsor-logo" />
           <img src={s5} alt="Sponsor S5" className="sponsor-logo" />
           <img src={s6} alt="Sponsor S6" className="sponsor-logo" />
